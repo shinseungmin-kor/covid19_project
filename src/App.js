21eea834cad1d9bb75d3d8ae818386b20456309e
shinseungmin-kor <shinseungmin10@gmail.com>
2021-09-26 23:56:25 +0900

    21.09.26

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import Header from "./components/Header";
+import Contents from "./components/Contents";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <section>
-        <h2>국내 코로나 현황</h2>
-        <div className="contents">
-          
-        </div>
-      </section>
+      <Contents />
     </div>
   );
 }
